9eb8e96f03ba5091107c253cac77271a7c59042d
Alsu <rakhmanova_2003@inbox.ru>
2024-06-27 14:11:52 +0300

    all screens

--- main.js
+++ main.js
@@ -95,3 +95,16 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".header__menu");
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+})
+
+document.querySelectorAll('.header__menu-link').forEach(n => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}))
